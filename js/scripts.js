7ceddd39be9633fc7d9bb8a157daaec4d33647b5
Yazmin Perez <zoeperez333@gmail.com>
2022-02-09 13:36:51 -0800

    add readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,30 @@
-$(document).ready(function() {
-$('form#wordPlay').submit(function(event) {
-  event.preventDefault();
+$(document).ready(function () {
+  $("form#wordPlay").submit(function (event) {
+    event.preventDefault();
 
-const sentenceOne = $('input#sentenceOne').val();
-$('.sentenceOne').text(sentenceOne);
+    const sentenceOne = $("input#sentenceOne").val();
+    $(".sentenceOne").text(sentenceOne);
 
-$('#display').show();
-$('form#wordPlay').hide();
+    $("#display").show();
+    $("form#wordPlay").hide();
 
-// the user will insert a sentence and it will become an array
-let strArray = sentenceOne.split(' ')
-let emptyArray = [];
+    // the user will insert a sentence and it will become an array
+    let strArray = sentenceOne.split(" ");
+    let emptyArray = [];
 
+    strArray.forEach(function (str) {
+      if (strArray.length >= 3) {
+        emptyArray.push(str);
+      }
+    });
 
-strArray.forEach(function(str) {
-  if (strArray.length >= 3) {
-emptyArray.push(str);
+    //reverse the sentence
+    emptyArray.reverse();
+    //join the sentence
+    let finalSentence = emptyArray.join(" ");
 
-  }
-});
-
-//reverse the sentence
-emptyArray.reverse();
-//join the sentence
-let finalSentence = emptyArray.join(' ');
-
-
-//print
-$('#display').append(finalSentence);
-console.log(finalSentence);
-});
+    //print
+    $("#display").append(finalSentence);
+    console.log(finalSentence);
+  });
 });
